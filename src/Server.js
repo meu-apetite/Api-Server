6ceb9a30c7a696ffb9c4e338d9122f94351b5698
gneris177 <gneris177@gmail.com>
2022-07-01 23:04:34 -0300

    Estrutura inicial

---
+++ src/Server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from "path";
+import { fileURLToPath } from 'url';
+
+
+class Server {
+  app = express();
+  PORT = 5000;
+
+  start() {
+    this.config();
+    this.route();
+
+    this.app.listen(this.PORT, () => console.log('server on'));
+  }
+
+  config() {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    this.app.set('views', path.join(__dirname, 'views'))
+    this.app.set("view engine", "ejs");
+  }
+
+  route() {
+    this.app.get("/", (req, res) => {
+      res.render("index");
+    })
+  }
+}
+
+export default Server;
