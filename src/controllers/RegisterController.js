b02305323d087ade83ad1f94dcfddcfac606c99d
gneris177 <gneris177@gmail.com>
2022-07-30 01:32:03 -0300

    Feat - Compactando o cadastro de usuários

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -1,4 +1,4 @@
-import Store from '../models/StoreModel.js';
+import Company from '../models/CompanyModel.js';
 import bcrypt from 'bcryptjs';
 
 export default class RegisterController {
@@ -7,23 +7,41 @@ export default class RegisterController {
   }
 
   async register(req, res) {
+    const { nameOwner, nameCompany, subdomain, email, password } = req.body;
+
+    //Minimum eight characters, at least one letter and one number:
+    const validationPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    if (!validationPassword.teste(password)) {
+      return res.render('register', {
+        messages: [
+          {
+            type: 'error',
+            text:
+              'A senha precisa ter o mínimo de oito caracteres, pelo menos uma letra e um número.',
+          },
+        ],
+      });
+    }
+
     try {
-      const { nameOwner, name, email, password } = req.body;
       const passwordHash = bcrypt.hashSync(password, 12);
 
-      const store = await Store.create({
+      const company = await Company.create({
         nameOwner,
-        name,
+        nameCompany,
+        subdomain,
         email,
-        passwordHash,
+        password: passwordHash,
       });
 
       const message = 'Conta criada, entre para continuar!';
-      const nextUrl = `/login?message=${message}`;
+      const nextUrl = `${company.subdomain}/login?message=${message}`;
 
       return res.redirect(nextUrl);
     } catch (error) {
       return res.render('register', {
+        inputData: { nameOwner, nameCompany, subdomain, email },
         messages: [
           { type: 'error', text: 'Erro: confira os campos e tente novamente.' },
         ],
